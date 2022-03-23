c54c2e77157876b569368a8622a03e52877bc068
alvin-tosh <alvingitonga14@gmail.com>
2022-03-23 13:52:22 +0300

    initial comit

---
+++ Assets/ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public Rigidbody ball_Rigidbody;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        ball_Rigidbody.AddForce(-transform.forward * 2500f);
+        
+    }
+
+    void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.name == "Cylinder") {
+            print("You lose!!");
+        }
+    }
+        
+}
+
+    
+
